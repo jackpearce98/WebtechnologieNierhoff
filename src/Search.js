67fd7d896253bcbd5a4e0dff875858a209349f5c
JarlNuramon <saiedsadeghz@gmail.com>
2019-05-05 13:30:10 +0200

    Added all js for start page

---
+++ src/Search.js
@@ -0,0 +1,6 @@
+import React from "react";
+export class Search extends React.Component {
+  render() {
+    return <div />;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./Post.json";
 import "./styles.css";
 import { Feed } from "./Feed";
+import { Header } from "./Header";
+import { Search } from "./Search";
 class App extends React.Component {
   render() {
     return (
       <div class="App">
+        <Header />
+        <Search />
         <Feed />
       </div>
     );
